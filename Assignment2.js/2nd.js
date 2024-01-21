d377f134634bccd2472e173bfd79cc7776001a20
mahmoodshaikh13 <156595532+mahmoodshaikh13@users.noreply.github.com>
2024-01-21 16:35:36 +0500

    Add files via upload

---
+++ Assignment2.js/2nd.js
@@ -0,0 +1,2 @@
+alert("Welcome to Our Website!\n\n" +
+"Assalamualaikum! We're excited to have you on our webpage.");
---
+++ Assignment3.js/3rd.js
@@ -0,0 +1,4 @@
+let length = 10;
+let width = 5;
+let area = length * width;
+console.log("The area of the rectangle is:", area);
